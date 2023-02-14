a0b960be6ad822aaf7127d3e9c3c9b526705b283
Eltaj0713 <eltacagazade2001@gmail.com>
2023-02-14 17:54:09 +0400

    dscdc

---
+++ src/main/java/com/example/demoexample/Config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.demoexample.Config;
+
+
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+//@Configuration
+//@EnableWebSecurity
+//
+//public class SecurityConfig {
+//
+//
+//
+//
+//
+//
+//}
